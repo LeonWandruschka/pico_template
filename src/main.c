6c13650350f61f584ad77c4eb79243fb9196f0fc
Leon Wandruschka <leon.wandruschka@outlook.com>
2025-02-16 16:56:41 +0100

    Only needed Headers

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 /*
-* This file contains main functions needed
-* for the LocalBus communication protokol.
-* The Localbus is a communication protokol 
-* based on a UART connection between one master
-* and multiple slaves.  
-* The UART can run up to 48 Mbaud if the processor
-* can handle it. 
-* For slow communications 115200 baud is recommended.
+ * main.c
+ *
+ * Simple example program to blink an LED on the Raspberry PI Pico
+ * and communicate over the USB serial port
 */
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include <pico/stdio.h>
+#include <pico/time.h>
+#include <hardware/gpio.h>
+
 
 const uint LED_PIN = 25;
 
